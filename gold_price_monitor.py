2b26000b7b1cd8cf8faf1bff0aea415a5cfa8167
Elevoros <tkaravas@gmail.com>
2025-08-08 10:26:57 +0300

    Update gold_price_monitor.py

--- gold_price_monitor.py
+++ gold_price_monitor.py
@@ -19,10 +19,16 @@ BOG_PRICES_PAGE = 'https://www.bankofgreece.gr/en/main-tasks/markets/gold/gold-p
 # Headers to make the request appear as if it's coming from a browser
 # This helps to avoid 403 Forbidden errors and cache issues.
 HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'DNT': '1',
     'Cache-Control': 'no-cache',
     'Pragma': 'no-cache'
 }
+
 
 def get_latest_bulletin_url():
     try:
